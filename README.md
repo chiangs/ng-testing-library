3994d91ff3a13cb3eb0fb954383f7556a1bcc7b0
Stephen Chiang <stephen.e.chiang@gmail.com>
2020-01-14 11:03:46 +0100

    Added Jest-Schematic and Angular-Testing-Library, updated README

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.22.
 
+Jest configuration via schematics from: [Jest-Schematic](https://github.com/briebug/jest-schematic) ***Thanks for this!***
+
+Integrated Testing-Library for Angular: [Testing-Library](https://testing-library.com/)
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
@@ -16,7 +20,11 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 
 ## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Run `npm test` to execute the unit tests via [Jest](https://jestjs.io/).
+
+Run `npm test -- --watch` to execute test with `--watch` mode on.
+
+Run `npm test -- --coverage` to get a coverage report with tests.
 
 ## Running end-to-end tests
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { render } from '@testing-library/angular';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -30,6 +27,13 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng8-testing-library app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'ng8-testing-library app is running!'
+    );
+  });
+
+  it('should render title', async () => {
+    const component = await render(AppComponent);
+    component.getByText('Here are some links to help you start:');
   });
 });
