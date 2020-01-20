afb9b2df6d2f6a1c748d502e976892752ce7d990
Stephen Chiang <stephen.e.chiang@gmail.com>
2020-01-20 14:08:01 +0100

    TS version bumped to 3.7 for Immer

--- src/app/core/core-store/core-state-reducer.ts
+++ src/app/core/core-store/core-state-reducer.ts
@@ -1,26 +1,39 @@
 import { initialState, CoreState } from './core-state.state';
 import { Actions, ActionTypes } from './core-store.actions';
+import produce, { Draft } from 'immer';
 
 const updateProps = (state: CoreState, props: any): CoreState => ({
   ...state,
   ...props
 });
 
-const verb1 = (state, action): CoreState => {
-  const updatedProps = {
-    testProp: `updated`
-  };
-  return updateProps(state, updatedProps);
-};
+// const verb1 = (state, action): CoreState => {
+//   const updatedProps = {
+//     testProp: `updated`
+//   };
+//   return updateProps(state, updatedProps);
+// };
 
-export const coreReducer = (
-  state: CoreState = initialState,
-  action: Actions
-) => {
-  switch (action.type) {
-    case ActionTypes.Verb1:
-      return verb1(state, action);
-    default:
-      return state;
-  }
-};
+// export const coreReducer = (
+//   state: CoreState = initialState,
+//   action: Actions
+// ) => {
+//   switch (action.type) {
+//     case ActionTypes.Verb1:
+//       return verb1(state, action);
+//     default:
+//       return state;
+//   }
+// };
+
+// * Immer way
+export const coreReducer = produce(
+  (draft: Draft<CoreState>, action: Actions) => {
+    switch (action.type) {
+      case ActionTypes.Verb1:
+        draft.testProp = action.payload;
+        return;
+    }
+  },
+  initialState
+);
